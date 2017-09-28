df1d32257f0898f5789f62ac7d7f438c628fcbb2
Maxime Mangel <maxime.shadaen@protonmail.com>
2017-09-28 16:44:13 +0200

    Fix gif and add reactNative show code

--- docs/content/index.fsx
+++ docs/content/index.fsx
@@ -11,7 +11,7 @@ Elmish applications can benefit from Hot Module Replacement (known as HMR).
 
 This allow us to modify the application while it's running, without a full reload. Your application will now maintain its state between two changes.
 
-![hmr demo](https://github.com/zalmoxisus/remotedev/raw/master/docs/files/img/hmr_demo.gif)
+![hmr demo](https://fable-elmish.github.io/hmr/img/hmr_demo.gif)
 
 
 ### Installation
@@ -24,7 +24,7 @@ paket add nuget Fable.Elmish.HMR
 ### Program module functions
 Augment your program instance with HMR support.
 
-If you are using Elmish with react, make sure to add the HMR support before `withReact` line.
+Make sure to add HMR support before `Program.withReact` or `Program.withReactNative`
 
 Usage:
 *)
@@ -34,11 +34,23 @@ open Elmish.HMR
 
 Program.mkProgram init update view
 |> Program.withHMR // Add the HMR support
-|> Program.withReact "elmsih-app"
+|> Program.withReact "elmish-app"
 |> Program.run
 
 (**
 
+and if you use React Native:
+*)
+
+
+open Elmish.HMR
+
+Program.mkProgram init update view
+|> Program.withHMR // Add the HMR support
+|> Program.withReactNative "elmish-app"
+|> Program.run
+
+(**
 ### Conditional compilation
 If don't want to include the debugger in production builds surround it with `#if DEBUG`/`#endif` and define the symbol conditionally in your build system.
 
