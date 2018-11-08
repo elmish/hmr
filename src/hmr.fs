10e4d0b0bc063a42e1a49a30186daef8dc331dff
Maxime Mangel <maxime.shadaen@protonmail.com>
2018-11-08 22:30:37 +0100

    Shadow Fable.Elmish.React functions

--- src/hmr.fs
+++ src/hmr.fs
@@ -3,6 +3,7 @@ namespace Elmish.HMR
 open Elmish
 open Elmish.Browser
 open Fable.Import
+open Fable.Import.Browser
 open Fable.Core.JsInterop
 
 [<RequireQualifiedAccess>]
@@ -149,4 +150,56 @@ You should not see this message
         (program : Program<'a,'model,'msg,'view>) =
 
             Navigation.Program.toNavigable parser urlUpdate program
+    #endif
+
+    #if DEBUG
+    /// Setup rendering of root React component inside html element identified by placeholderId
+    let withReact placeholderId (program:Elmish.Program<_,_,_,_>) =
+        let mutable lastRequest = None
+        let setState model dispatch =
+            match lastRequest with
+            | Some r -> window.cancelAnimationFrame r
+            | _ -> ()
+
+            lastRequest <- Some (window.requestAnimationFrame (fun _ ->
+                Fable.Import.ReactDom.render(
+                    lazyView2With (fun x y -> obj.ReferenceEquals(x,y)) program.view model dispatch,
+                    document.getElementById(placeholderId)
+                )))
+
+        { program with setState = setState }
+    #else
+    let inline withReact placeholderId (program:Elmish.Program<_,_,_,_>) =
+        Elmish.React.Common.withReact placeholderId program
+    #endif
+
+    #if DEBUG
+    /// `withReact` uses `requestAnimationFrame` to optimize rendering in scenarios with updates at a higher rate than 60FPS, but this makes the cursor jump to the end in `input` elements.
+    /// This function works around the glitch if you don't need the optimization (see https://github.com/elmish/react/issues/12).
+    let withReactUnoptimized placeholderId (program:Elmish.Program<_,_,_,_>) =
+        let setState model dispatch =
+            Fable.Import.ReactDom.render(
+                lazyView2With (fun x y -> obj.ReferenceEquals(x,y)) program.view model dispatch,
+                document.getElementById(placeholderId)
+            )
+
+        { program with setState = setState }
+    #else
+    let inline withReactUnoptimized placeholderId (program:Elmish.Program<_,_,_,_>) =
+        Elmish.React.Common.withReactUnoptimized placeholderId program
+    #endif
+
+    #if DEBUG
+    /// Setup rendering of root React component inside html element identified by placeholderId using React.hydrate
+    let withReactHydrate placeholderId (program:Elmish.Program<_,_,_,_>) =
+        let setState model dispatch =
+            Fable.Import.ReactDom.hydrate(
+                lazyView2With (fun x y -> obj.ReferenceEquals(x,y)) program.view model dispatch,
+                document.getElementById(placeholderId)
+            )
+
+        { program with setState = setState }
+    #else
+    let inline withReactHydrate placeholderId (program:Elmish.Program<_,_,_,_>) =
+        Elmish.React.Common.withReactHydrate placeholderId program
     #endif
